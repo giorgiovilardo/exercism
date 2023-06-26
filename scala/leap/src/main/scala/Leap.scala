f1a9c0ec3cc5f21fa85714a686dfb13e17744aef
Giorgio Vilardo <giorgio@vilardo.eu>
2023-06-26 23:19:37 +0200

    solver scala leap

--- scala/leap/src/main/scala/Leap.scala
+++ scala/leap/src/main/scala/Leap.scala
@@ -1,3 +1,8 @@
 object Leap {
-  def leapYear(year: Int): Boolean = ???
+  def leapYear(year: Int): Boolean =
+    year match {
+      case leap if leap % 400 == 0 => true
+      case leap if leap % 4 == 0 && leap % 100 != 0 => true
+      case _ => false
+    }
 }
