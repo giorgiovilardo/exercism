97248d4c0f50c18ed66dd74688a90b15ee07003f
Giorgio Vilardo <giorgio@vilardo.eu>
2023-06-27 13:21:12 +0200

    solver scala gradeschool

--- scala/grade-school/src/main/scala/GradeSchool.scala
+++ scala/grade-school/src/main/scala/GradeSchool.scala
@@ -1,12 +1,17 @@
 class School {
   type DB = Map[Int, Seq[String]]
 
-  def add(name: String, g: Int) = ???
+  private var _db: DB = Map[Int, Seq[String]]()
 
-  def db: DB = ???
+  def add(name: String, grade: Int) = {
+    _db = db + (grade -> (db.getOrElse(grade, Seq()) :+ name))
+    _db
+  }
 
-  def grade(g: Int): Seq[String] = ???
+  def db: DB = _db
 
-  def sorted: DB = ???
+  def grade(g: Int): Seq[String] = _db.getOrElse(g, Seq())
+
+  def sorted: DB = _db.toSeq.map(tuple => (tuple._1, tuple._2.sorted)).sortBy(_._1).toMap
 }
 
