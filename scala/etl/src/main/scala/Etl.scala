62f482585491568f88819fe941ac8e0dd9603e9e
Giorgio Vilardo <giorgio@vilardo.eu>
2023-06-27 22:43:19 +0200

    solve scala etl

--- scala/etl/src/main/scala/Etl.scala
+++ scala/etl/src/main/scala/Etl.scala
@@ -1,3 +1,4 @@
 object Etl {
-  def transform(scoreMap: Map[Int, Seq[String]]): Map[String, Int] = ???
+  def transform(scoreMap: Map[Int, Seq[String]]): Map[String, Int] =
+    scoreMap.flatMap{ case (k, v) => v.map(_.toLowerCase -> k) }
 }
