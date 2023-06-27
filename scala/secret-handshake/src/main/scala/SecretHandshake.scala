23e28400cb65d3c9ded9410ab83c6a457472bc9c
Giorgio Vilardo <giorgio@vilardo.eu>
2023-06-28 00:25:02 +0200

    solve scala secrethandshake

--- scala/secret-handshake/src/main/scala/SecretHandshake.scala
+++ scala/secret-handshake/src/main/scala/SecretHandshake.scala
@@ -1,3 +1,11 @@
 object SecretHandshake {
-  def commands(v: Int): List[String] = ???
+  private val bitFieldsValue = List("invert", "jump", "close your eyes", "double blink", "wink")
+  private def commandParser(commandString: String) = {
+    commandString.zip(bitFieldsValue).collect { case (flag, command) if flag == '1' => command }.reverse.toList
+  }
+
+  def commands(v: Int): List[String] = {
+    val commands = commandParser(v.toBinaryString.reverse.padTo(5, '0').reverse)
+    if (commands.contains("invert")) commands.filter(_ != "invert").reverse else commands
+  }
 }
