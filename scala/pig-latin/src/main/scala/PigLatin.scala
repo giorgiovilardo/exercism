405cc5b5dbb590f587b573b0534450b8ce54ff6f
Giorgio Vilardo <giorgio@vilardo.eu>
2023-06-27 22:06:56 +0200

    solve scala piglatin

--- scala/pig-latin/src/main/scala/PigLatin.scala
+++ scala/pig-latin/src/main/scala/PigLatin.scala
@@ -1,3 +1,16 @@
 object PigLatin {
-  def translate(s: String): String = ???
+  private val startsWithConsonant = "([^aeiou]*)(.*)".r
+  private val startsWithConsonantFollowedByQu = "([^aeiou]*qu)(.*)".r
+  private val startsWithVowelSound = "([aeiou]|xr|yt)(.*)".r
+  private val hasYAsSecondConsonantBlock = "([^aeiouy]+)(y.*)".r
+
+  def translate(s: String): String = {
+    s.split(' ').map {
+      case startsWithVowelSound(x, xs) => s"${x}${xs}ay"
+      case hasYAsSecondConsonantBlock(x, xs) => s"$xs${x}ay"
+      case startsWithConsonantFollowedByQu(x, xs) => s"$xs${x}ay"
+      case startsWithConsonant(x, xs) => s"$xs${x}ay"
+      case _ => s
+    }.mkString(" ")
+  }
 }
