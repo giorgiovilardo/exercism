e0f87cb2cb20e5b8b83bb1f49d58283d4cc328f1
Giorgio Vilardo <giorgio@vilardo.eu>
2023-06-27 14:23:21 +0200

    solve scala bob

--- scala/bob/src/main/scala/Bob.scala
+++ scala/bob/src/main/scala/Bob.scala
@@ -1,3 +1,18 @@
 object Bob {
-  def response(statement: String): String = ???
+  implicit class StringExtensions(val s: String) {
+    def isUpper: Boolean = {
+      val onlyLetters = s.filter(_.isLetter)
+      onlyLetters.toUpperCase == onlyLetters && onlyLetters.nonEmpty
+    }
+  }
+
+  def response(statement: String): String = {
+    statement.trim match {
+      case s"${sentence}?" if sentence.isUpper => "Calm down, I know what I'm doing!"
+      case s"${_}?" => "Sure."
+      case sentence if sentence.isUpper => "Whoa, chill out!"
+      case sentence if sentence.filterNot(_.isWhitespace) == "" => "Fine. Be that way!"
+      case _ => "Whatever."
+    }
+  }
 }
