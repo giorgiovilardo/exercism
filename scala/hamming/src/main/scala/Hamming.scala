12825c4b22ad786ad9a4509ed2899ab96479f7f3
Giorgio Vilardo <giorgio@vilardo.eu>
2023-06-27 22:18:16 +0200

    solve scala hamming

--- scala/hamming/src/main/scala/Hamming.scala
+++ scala/hamming/src/main/scala/Hamming.scala
@@ -1,3 +1,6 @@
 object Hamming {
-  def distance(dnaStrandOne: String, dnaStrandTwo: String): Option[Int] = ???
+  def distance(dnaStrandOne: String, dnaStrandTwo: String): Option[Int] = {
+    if (dnaStrandOne.length != dnaStrandTwo.length) None
+    else Some(dnaStrandOne.zip(dnaStrandTwo).count(pair => pair._1 != pair._2))
+  }
 }
