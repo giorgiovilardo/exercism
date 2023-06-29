0b80a3f0c6e99ef60a03d2995d6c1265ae9ffe47
Giorgio Vilardo <giorgio@vilardo.eu>
2023-06-29 17:14:28 +0200

    scala wip customset

--- scala/custom-set/src/main/scala/CustomSet.scala
+++ scala/custom-set/src/main/scala/CustomSet.scala
@@ -1,15 +1,23 @@
-case class CustomSet[A](private val db: List[A] = List()) {
-}
-object CustomSet {
-  def empty[A](s: CustomSet[A]): Boolean = ???
-  def fromList[A](l: List[A]): CustomSet[A] = ???
-  def member[A](s: CustomSet[A], i: A): Boolean = ???
-  def insert[A](s: CustomSet[A], i: A): CustomSet[A] = ???
-  def intersection[A](s: CustomSet[A], other: CustomSet[A]): CustomSet[A] = ???
-  def difference[A](s: CustomSet[A], other: CustomSet[A]): CustomSet[A] = ???
-  def union[A](s: CustomSet[A], other: CustomSet[A]): CustomSet[A] = ???
-  def isSubsetOf[A](s: CustomSet[A], other: CustomSet[A]): Boolean = ???
-  def isDisjointFrom[A](s: CustomSet[A], other: CustomSet[A]): Boolean = ???
-  def isEqual[A](s: CustomSet[A], other: CustomSet[A]): Boolean = ???
-}
+case class CustomSet[A](private val db: scala.collection.immutable.Map[A, Boolean] = scala.collection.immutable.Map[A, Boolean]()) {}
 
+object CustomSet {
+  def empty[A](s: CustomSet[A]): Boolean = s.db.isEmpty
+
+  def fromList[A](l: List[A]): CustomSet[A] = CustomSet(Map.from(l.map(_ -> false)))
+
+  def member[A](s: CustomSet[A], i: A): Boolean = s.db.contains(i)
+
+  def insert[A](s: CustomSet[A], i: A): CustomSet[A] = s.copy(db = s.db + (i -> false))
+
+  def intersection[A](s: CustomSet[A], other: CustomSet[A]): CustomSet[A] = s.copy(db = s.db.filter(kv => other.db.contains(kv._1)))
+
+  def difference[A](s: CustomSet[A], other: CustomSet[A]): CustomSet[A] = s.copy(db = s.db.filterNot(kv => other.db.contains(kv._1)))
+
+  def union[A](s: CustomSet[A], other: CustomSet[A]): CustomSet[A] = s.copy(db = s.db ++ other.db)
+
+  def isSubsetOf[A](s: CustomSet[A], other: CustomSet[A]): Boolean = s.db.forall(kv => other.db.contains(kv._1))
+
+  def isDisjointFrom[A](s: CustomSet[A], other: CustomSet[A]): Boolean = s.db.forall(kv => !other.db.contains(kv._1))
+
+  def isEqual[A](s: CustomSet[A], other: CustomSet[A]): Boolean = s.db == other.db
+}
