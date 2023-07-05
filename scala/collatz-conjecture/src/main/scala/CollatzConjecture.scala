a02251128dbfab60ce45575c78b8b3f2839888fb
Giorgio Vilardo <giorgio@vilardo.eu>
2023-07-05 23:29:43 +0200

    solve scala collatzconjecture

--- scala/collatz-conjecture/src/main/scala/CollatzConjecture.scala
+++ scala/collatz-conjecture/src/main/scala/CollatzConjecture.scala
@@ -1,3 +1,14 @@
 object CollatzConjecture {
-  def steps(s: Int): Int = ???
+  def steps(s: Int): Option[Int] = {
+    if (s <= 0) None
+    else Some(Iterator.unfold(s) {
+      case 1 => None
+      case nonOne =>
+        val calc = collatzAlgorithm(nonOne)
+        Some((calc, calc))
+    }.length)
+  }
+
+  private def collatzAlgorithm(n: Int): Int =
+    if (n % 2 == 0) n / 2 else n * 3 + 1
 }
