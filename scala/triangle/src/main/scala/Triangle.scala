5424200aac20d62b565a6fae79443a154d8eb18e
Giorgio Vilardo <giorgio@vilardo.eu>
2023-07-06 12:32:42 +0200

    solve scala triangle

--- scala/triangle/src/main/scala/Triangle.scala
+++ scala/triangle/src/main/scala/Triangle.scala
@@ -1,7 +1,14 @@
 case class Triangle(a: Double, b: Double, c: Double) {
-  def equilateral: Boolean = ???
-  def isosceles: Boolean = ???
-  def scalene: Boolean = ???
+  private def sidesSet = Set() + a + b + c
+  private def isLegal = {
+    val sides = List(a,b,c).zipWithIndex
+    val allMoreThanZero = sides.forall(_._1 > 0)
+    val allRespectsTriangleInequality = sides.map(item => (item._1, sides.filter(tuple => tuple._2 != item._2).map(_._1).sum)).forall(s => s._1 < s._2)
+    allMoreThanZero && allRespectsTriangleInequality
+  }
+  def equilateral: Boolean = sidesSet.size == 1 && isLegal
+  def isosceles: Boolean = sidesSet.size != 3 && isLegal
+  def scalene: Boolean = sidesSet.size == 3 && isLegal
 }
 object Triangle {
   def apply(a: Int, b: Int, c: Int): Triangle = Triangle(a.toDouble, b.toDouble, c.toDouble)
