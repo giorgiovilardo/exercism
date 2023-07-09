58258d1c679c289ac084836732eceb90e8c47e0c
Giorgio Vilardo <giorgio@vilardo.eu>
2023-07-09 18:39:15 +0200

    solve scala bankaccount

--- scala/bank-account/src/main/scala/BankAccount.scala
+++ scala/bank-account/src/main/scala/BankAccount.scala
@@ -1,13 +1,21 @@
-trait BankAccount {
-
+sealed trait BankAccount {
   def closeAccount(): Unit
-
   def getBalance: Option[Int]
-
   def incrementBalance(increment: Int): Option[Int]
 }
 
-object Bank {
-  def openAccount(): BankAccount = ???
+case class Account() extends BankAccount {
+  private var balance: Option[Int] = Some(0)
+  override def closeAccount(): Unit = balance = None
+  override def getBalance: Option[Int] = balance
+  override def incrementBalance(increment: Int): Option[Int] = {
+    synchronized {
+      balance = balance.map(bal => bal + increment)
+      balance
+    }
+  }
 }
 
+object Bank {
+  def openAccount(): BankAccount = Account()
+}
