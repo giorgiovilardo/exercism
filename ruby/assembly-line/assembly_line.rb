d4f0b5bba3539d31027e71ad32277ddc581c35bb
Giorgio Vilardo <giorgio@vilardo.eu>
2023-11-18 19:17:41 +0100

    finish assembly ruby

--- ruby/assembly-line/assembly_line.rb
+++ ruby/assembly-line/assembly_line.rb
@@ -1,13 +1,29 @@
 class AssemblyLine
+  CAR_PER_HOUR = 221
+
   def initialize(speed)
     @speed = speed
+    @fault_modifier = calculate_modifier(speed)
   end
 
   def production_rate_per_hour
-    raise 'Please implement the AssemblyLine#production_rate_per_hour method'
+    Float(@speed * CAR_PER_HOUR * @fault_modifier)
   end
 
   def working_items_per_minute
-    raise 'Please implement the AssemblyLine#working_items_per_minute method'
+    (production_rate_per_hour / 60).floor
+  end
+
+  def calculate_modifier(value)
+    case value
+    when 1..4
+      return 1
+    when 5..8
+      return 0.9
+    when 9
+      return 0.8
+    when 10
+      return 0.77
+    end
   end
 end
